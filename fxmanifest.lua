ff3e0b11b008040cc438f6c7d8c06e875e570c24
Bandzukáč <128176740+Bandzukac@users.noreply.github.com>
2023-04-20 18:47:14 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
-fx_version 'adamant' 
+fx_version   'cerulean'
 game 'gta5' 
 
 author 'Bandzukáč#1658' 
 description 'Made by Bandzukáč#1658 / discord: https://discord.gg/5Zj3pxbSDu' -- contact
 version '1.0.0'
 
-client_scripts 'client.lua'
+client_script 'client.lua'
 
